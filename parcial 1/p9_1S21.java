73ee8e70c667627f44ff73b49424fecf133e443a
AxlJimenez <144179033+AxlJimenez@users.noreply.github.com>
2023-10-10 14:39:18 -0600

    Add files via upload

---
+++ parcial 1/p9_1S21.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class p9_1S21{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+        String operacion;
+        System.out.println("Que tipo de operacion desea realicar");
+        System.out.println("Escribala: Suma, Resta, Multiplicacion, Division");
+        operacion = teclado.nextLine();
+
+        if (operacion.equals("Suma")){
+            System.out.println("Haremos una suma");
+        
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("ingresa 2 numeros ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a + b;
+    System.out.println("La suma de los 2 numeros es: " + c);
+    input.close();
+}
+
+    else if (operacion.equals("Resta")){
+    System.out.println("Haremos una Resta");
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("ingresa 2 numeros ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a - b ;
+        System.out.println("La resta de los 2 numeros es " + c);
+        input.close();
+
+    }
+     else if (operacion.equals("Multiplicacion")){
+    System.out.println("Haremos una Multiplicacion");
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("ingresa 2 numeros ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a * b;
+        System.out.println("La Multiplicacion de los 2 numeros es " + c);
+        input.close();
+
+    }
+
+else if (operacion.equals("Division")){
+    System.out.println("Haremos una Division");
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("ingresa 2 numeros ");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a / b;
+        System.out.println("La Division de los 2 numeros es " + c);
+        input.close();
+}
+
+else {
+    System.out.print("No existe la operacion");
+    
+}
+
+}
+}
+
+
+
+
+
+
+
