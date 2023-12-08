8597ebe3810edb9df749a254ca33b604d361eae1
AxlJimenez <144179033+AxlJimenez@users.noreply.github.com>
2023-12-08 09:47:15 -0600

    Add files via upload

--- Parcial 3/P3p9_1S21.java
+++ Parcial 3/P3p9_1S21.java
@@ -1,51 +1,56 @@
 import java.util.Scanner;
-public class P3p9_1S21{
 
+public class P3p9_1S21 {
+    static int n, i, cubo, factorialn, factoriali;
+    static int[] valoresFactorialI; 
+    static int sumatoria;
 
-    static int i,n,a,cubo,multiplicacion,sumatoria;
-    static int factorialn,factoriali;
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa el valor de n ");
+        n = teclado.nextInt();
+        System.out.println("Ingresa el factorial de i");
+        i = teclado.nextInt();
 
+        cubo();
+        factoriales();
+        multiplicacion();
+        sumatoria();
 
-	public static void main(String[] args) {
-		Scanner teclado=new Scanner(System.in);
-		System.out.println("Ingresa el valor de n ");
-		n=teclado.nextInt();
-		System.out.println("Ingresa el factorial de i" );
-		i=teclado.nextInt();
-		System.out.println(factorialn);
-	}
-
-
-
-	public static void factoriales(){
-		for(i=0;i<n;i++){
-		    factorialn=1;
-		    if (i==0) factorialn=1;
-		    else{
-		    	for (a=i;a>0;a--){
-		    		factorialn*=a;
-		    	}
-		    }
-		
+        System.out.println("La Sumatoria es de  " + sumatoria);
     }
+
+    public static void factoriales() {
+        factorialn = 1;
+        factoriali = 1;
+
+        for (int j = 1; j <= n; j++) {
+            factorialn *= j;
+        }
+
+        for (int j = 1; j <= i; j++) {
+            factoriali *= j;
+        }
     }
-  
 
-		
+    public static void cubo() {
+        cubo = n * n * n;
+    }
 
+    public static void multiplicacion() {
+        valoresFactorialI = new int[]{1, 2, 3}; 
 
+        for (int j = 0; j < valoresFactorialI.length; j++) {
+            valoresFactorialI[j] *= cubo;
+        }
+    }
 
-	public static void cubo(){
-		cubo=n*n*n;
+    public static void sumatoria() {
+        sumatoria = 0;
 
-	}
+        for (int j = 0; j < valoresFactorialI.length; j++) {
+            sumatoria += valoresFactorialI[j] / factorialn;
+        }
+    }
+}
 
-	public static void multiplicacion(){
-		multiplicacion=factoriali*cubo;
-	}
-
-
-	public static void sumatoria(){
-		sumatoria=multiplicacion/factorialn;
-	}
-} 
